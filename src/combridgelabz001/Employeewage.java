02f98352939d4f421a3e10f6e868700db72857fa
MEENAKSHI493 <Meenakshi493r@gmail.com>
2021-06-30 19:06:11 +0530

    employee wage

---
+++ src/combridgelabz001/Employeewage.java
@@ -0,0 +1,10 @@
+package combridgelabz001;
+
+public class Employeewage {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/module-info.java
@@ -0,0 +1,2 @@
+module javaoopsconcepts {
+}
