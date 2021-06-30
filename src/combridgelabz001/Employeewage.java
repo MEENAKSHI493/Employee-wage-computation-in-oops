721ace95cc12c29b471e1f6da6659baa924e91e6
MEENAKSHI493 <Meenakshi493r@gmail.com>
2021-06-30 19:17:52 +0530

    employeedaily wage

--- src/combridgelabz001/Employeewage.java
+++ src/combridgelabz001/Employeewage.java
@@ -1,12 +1,11 @@
 package combridgelabz001;
 public class Employeewage {
 	public static void main(String[] args){
-		checkAttendance employee = new checkAttendance();
-		employee.isPresent();
-	    }
-	}
-
-	class checkAttendance {
+			checkAttendance employee = new checkAttendance();
+			employee.isPresent();
+		    }
+		}
+	class checkAttendance extends calculateWage{
 		//Constants
 		int IS_FULL_TIME = 1;
 		//Computation
@@ -14,7 +13,16 @@ public class Employeewage {
 	    	void isPresent(){
 	        if ( empCheck == IS_FULL_TIME )
 	            System.out.println("Employee is Present ");
-	        else
+		    System.out.println("Wage is "+dailyWage());	
 	            System.out.println("Employee is Absent ");
 	    }
+	}
+
+	//Wage computation 
+	class calculateWage {
+	    	final int wagePerHr = 20;
+	    	final int workHr = 8;
+	    	int dailyWage(){
+	        return wagePerHr * workHr;
+	    }	
 }
